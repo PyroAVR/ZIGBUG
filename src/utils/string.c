63db5853ce584ed0cd15a5a46879ce3409b9b556
Andrew H. Meyer <ahm7197@rit.edu>
2022-09-26 22:53:22 -0700

    reference implementation string ops lib

--- src/utils/string.c
+++ src/utils/string.c
@@ -1,9 +1,13 @@
 #include <utils/string.h>
 
-#include <stdbool.h>
+#include <compiler_abstraction.h>
 
-WEAK char tolower(char c) {
-    if(c >= 'A' || c <= 'Z') {
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+char tolower(char c) {
+    if(c >= 'A' && c <= 'Z') {
         return c - 'A' + 'a';
     }
     else {
@@ -11,8 +15,8 @@ WEAK char tolower(char c) {
     }
 }
 
-WEAK char toupper(char c) {
-    if(c >= 'a' || c <= 'z') {
+char toupper(char c) {
+    if(c >= 'a' && c <= 'z') {
         return c - 'a' + 'A';
     }
     else {
@@ -20,9 +24,16 @@ WEAK char toupper(char c) {
     }
 }
 
-WEAK bool ishex(char c) {
-    c = tolower(c);
+char ishex(char c) {
     return ((c >= 'a' && c <= 'f') || (c >= '0' && c <= '9'));
+}
+
+char iscrlf(char c) {
+    return (c == '\r' || c == '\n');
+}
+
+char iswhitespace(char c) {
+    return (c == '\r' || c == '\n' || c == ' ' || c == '\t');
 }
 
 int hex2nibble(char c) {
@@ -34,7 +45,7 @@ int hex2nibble(char c) {
     }
 }
 
-static void int2hex(char *buf, char byte) {
+void int2hex(char *buf, char byte) {
     char lower = byte & 0x0f;
     char upper = (byte & 0xf0) >> 4;
     if(upper > 9) {
@@ -51,49 +62,7 @@ static void int2hex(char *buf, char byte) {
     }
 }
 
-static uintptr_t hex2int(char *buf, size_t len) {
-    char r = 0;
-    if(len > (sizeof(uintptr_t) << 1)) {
-        return 0;
-    }
-    for(int i = len-1; i >= 0; i--) {
-        if(!buf[i]) break;
-        r |= hex2nibble(buf[i]) << ((len - i - 1)*4);
-    }
-    return r;
-}
-
-static char ishex(char c) {
-    return ((c >= 'a' && c <= 'f') || (c >= '0' && c <= '9'));
-}
-
-static int hex2nibble(char c) {
-    if(c < 'A') {
-        return c - 'A';
-    }
-    else {
-        return c - '0';
-    }
-}
-
-static void int2hex(char *buf, char byte) {
-    char lower = byte & 0x0f;
-    char upper = (byte & 0xf0) >> 4;
-    if(upper > 9) {
-        buf[1] = (upper - 9) + 'A';
-    }
-    else {
-        buf[1] = upper + '0';
-    }
-    if(lower > 9) {
-        buf[0] = (lower - 9) + 'A';
-    }
-    else {
-        buf[0] = lower + '0';
-    }
-}
-
-static uintptr_t hex2int(char *buf, size_t len) {
+uintptr_t hex2int(char *buf, size_t len) {
     char r = 0;
     if(len > (sizeof(uintptr_t) << 1)) {
         return 0;
